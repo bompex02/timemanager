7dc05e266e1c9e60173e7e9dc7031f35e688f779
Marcus Baumgartner <m.baumgartner2002@gmx.de>
2025-03-28 00:07:54 +0100

    FIX #14: added 'preventDefault()' to logIn method + changed the button type from submit => button

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -11,7 +11,7 @@
           <div>
             <label for="email" class="block text-sm font-medium text-gray-900">Email</label>
             <div class="mt-2">
-              <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600 sm:text-sm" />
+              <input v-model="email" type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600 sm:text-sm" />
             </div>
           </div>
 
@@ -23,12 +23,12 @@
               </div>
             </div>
             <div class="mt-2">
-              <input type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600 sm:text-sm" />
+              <input v-model="password" type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600 sm:text-sm" />
             </div>
           </div>
 
           <div>
-            <button @click="logInUser" type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow hover:bg-indigo-500 focus:outline-indigo-600">Sign in</button>
+            <button @click="logInUser" type="button" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow hover:bg-indigo-500 focus:outline-indigo-600">Sign in</button>
           </div>
         </form>
 
@@ -53,10 +53,11 @@ const password = ref('')
 const router = useRouter()
 const authService = AuthService.getInstance()
 
-const logInUser = () => {
+const logInUser = (event: Event) => {
+    event.preventDefault(); // Prevent form submission
     // FOR DEBUG ONLY!
     authService.logInUser(email.value, password.value)
-    console.log('Login user' + email.value + ' ' + password.value)
+    console.log('Login user: ' + email.value + ' ' + password.value)
     router.push('/dashboard')
 }
 </script>
