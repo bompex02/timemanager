2b091ba9fa3769c60cbcb5fc4badb3c7c22ac0fa
1clone47 <andy.wagner1603@gmail.com>
2025-11-18 14:03:14 +0100

    #37: add base input design

---
+++ src/components/base/Input.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-[350px] px-1 flex flex-col" :class="[props.label ? 'pt-1' : 'pt-4']">
+        <label for="input" class="text-sm text-black">{{ props.label }}</label>
+        <input name="input" class="border-3 border-black rounded-lg p-1 w-full" />
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    label?: string
+}>()
+</script>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,15 +1,21 @@
 <template>
+ <div class="h-full flex flex-col gap-4">
   <div class="grid grid-cols-2 grid-rows-1 gap-4 w-full pt-4">
     <!-- Andere Charts -->
     <DailyWorkHoursChart />
     <ComparisonWorkhoursChart />
   </div>
   <AllRecordList class="col-span-2 row-span-2 w-full mt-4" />
+  <div>
+    <BaseInput label="test" />
+  </div>  
+ </div>
 </template>
   
 <script setup lang="ts">
   import AllRecordList from '../components/AllRecordList.vue';
   import DailyWorkHoursChart from '../components/DailyWorkHoursChart.vue';
   import ComparisonWorkhoursChart from '../components/ComparisonWorkhoursChart.vue';
+  import BaseInput from '@/components/base/Input.vue'
 </script>
  
