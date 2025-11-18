7edeb59cef30e32a85ea80b97c182ad7425381e1
1clone47 <andy.wagner1603@gmail.com>
2025-11-18 14:06:07 +0100

    #37: add model to input

--- src/components/base/Input.vue
+++ src/components/base/Input.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-[350px] px-1 flex flex-col" :class="[props.label ? 'pt-1' : 'pt-4']">
         <label for="input" class="text-sm text-black">{{ props.label }}</label>
-        <input name="input" class="border-3 border-black rounded-lg p-1 w-full" />
+        <input v-model="model" name="input" class="border-3 border-black rounded-lg p-1 w-full" />
     </div>
 </template>
 
@@ -9,4 +9,6 @@
 const props = defineProps<{
     label?: string
 }>()
+
+const model = defineModel<string | null>()
 </script>
