8480ce383b0b89924925e74a9a998735b693b5dd
Marcus Baumgartner <m.baumgartner2002@gmx.de>
2025-12-26 21:54:35 +0100

    package update

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,17 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 import 'primeicons/primeicons.css';
+import storage from 'vue3-storage'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(PrimeVue);
+
+app.use(storage({
+    namespace: 'app_', // prefix for all keys
+    storage: localStorage,
+    expire: 60 * 60 // 1h expiration
+}));
 
 app.mount('#app')
